9dcc476a24fc6d55817322a69932065907e8ccda
Dima work <thetekst@gmail.com>
2012-10-11 17:39:28 +0400

    last commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<?
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////Includ's
+require_once("php/adds.php");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////Objects
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////Globals variables
+
+?>
+<!doctype html>
+<html lang=ru>
+  <head>
+  
+	  <!--META's START-->
+      <meta charset=utf-8>
+	  <!--META's END-->
+	  
+      <title>HTML5</title>
+	  
+	  <!--CSS START-->
+      <link rel="stylesheet" href="./css/style.css" />
+	  <!--CSS END-->
+	  
+      <!--[if IE]>
+          <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+      <![endif]-->
+	  
+	  <!--Javascript START-->
+	  <script src="./js/jquery-1.8.2.min.js"></script>
+	  <script src="./js/script.js"></script>
+	  <!--Javascript END-->
+	  
+ </head>
+ <body>
+	<!--GlobalContainer START-->
+	<div id="GlobalContainer">
+	  
+	  <!--Header START-->
+      <header class="widthController">
+			<nav id="">
+				<table id="main_menu">
+					<tbody>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</nav>
+      </header>
+	  <!--Header END-->
+ 
+	  <!--Content START-->
+      <div id="content">
+           <div id="">Пост</div>
+           <aside id="sidebar_right">Боковая колонка</aside>
+      </div>
+	  <!--Content END-->
+	  
+	  <!--Footer START-->
+      <footer>
+		<div id=""></div>
+	  </footer>
+	  <!--Footer END-->
+	  
+	</div>
+	<!--GlobalContainer END-->
+	
+   </body>
+</html>
