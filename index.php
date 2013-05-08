7083dca2469a73ddf58a79bb6006998c15923c44
Dima work <thetekst@gmail.com>
2013-05-08 17:05:55 +0400

    align code

--- index.php
+++ index.php
@@ -12,68 +12,67 @@ require_once("php/adds.php");
 ?>
 <!doctype html>
 <html lang="ru">
-  <head>
-  
-	  <!--META's START-->
-      <meta charset="utf-8">
-	  <!--META's END-->
+	<head>
+		<!--META's BEGIN-->
+		<meta charset="utf-8">
+		<!--META's END-->
+
+		<title>HTML5</title>
+
+		<!--CSS BEGIN-->
+		<link rel="stylesheet" href="./css/style.css" />
+		<link rel="icon" type="image/x-icon" href="./favicon.ico" />
+		<!--CSS END-->
+
+		<!--[if IE]>
+		  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+		<!--Javascript BEGIN-->
+		<script src="./js/jquery-1.8.2.min.js"></script>
+		<script src="./js/script.js"></script>
+		<!--Javascript END-->
 	  
-      <title>HTML5</title>
-	  
-	  <!--CSS START-->
-      <link rel="stylesheet" href="./css/style.css" />
-	  <link rel="icon" type="image/x-icon" href="./favicon.ico" />
-	  <!--CSS END-->
-	  
-      <!--[if IE]>
-          <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-      <![endif]-->
-	  
-	  <!--Javascript START-->
-	  <script src="./js/jquery-1.8.2.min.js"></script>
-	  <script src="./js/script.js"></script>
-	  <!--Javascript END-->
-	  
- </head>
- <body>
-	<!--GlobalContainer START-->
-	<div id="GlobalContainer">
-	  
-	  <!--Header START-->
-      <header class="widthController">
-			<nav id="">
-				<table id="main_menu">
-					<tbody>
-						<tr>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-						</tr>
-					</tbody>
-				</table>
-			</nav>
-      </header>
-	  <!--Header END-->
- 
-	  <!--Content START-->
-      <div id="content">
-           <div id="">Пост</div>
-           <aside id="sidebar_right">Боковая колонка</aside>
-      </div>
-	  <!--Content END-->
-	  
-	  <!--Footer START-->
-      <footer>
-		<div id=""></div>
-	  </footer>
-	  <!--Footer END-->
-	  
-	</div>
-	<!--GlobalContainer END-->
-	
-   </body>
+	</head>
+	<body>
+		<!--GlobalContainer BEGIN-->
+		<div id="GlobalContainer">
+
+			<!--Header BEGIN-->
+			<header class="widthController">
+				<nav id="">
+					<table id="main_menu">
+						<tbody>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+				</nav>
+			</header>
+			<!--Header END-->
+
+			<!--Content BEGIN-->
+			<div id="content">
+				<div id="">Пост</div>
+				<aside id="sidebar_right">Боковая колонка</aside>
+			</div>
+			<!--Content END-->
+
+			<!--Footer BEGIN-->
+			<footer>
+				<div id=""></div>
+			</footer>
+			<!--Footer END-->
+
+		</div>
+		<!--GlobalContainer END-->
+
+	</body>
 </html>
