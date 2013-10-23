5f28ffc9938cf0cda9416cefe95b15e85f73fe67
Dima work <thetekst@gmail.com>
2013-10-23 10:19:06 +0400

    bitrix include file for section

--- cms/bitrix/footer.php
+++ cms/bitrix/footer.php
@@ -7,6 +7,17 @@
 				Array("MODE"=>"html")
 			);?>
 			<!--include file END-->
+			
+			<!--include file for section BEGIN-->
+			<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+				"AREA_FILE_SHOW" => "sect",
+				"AREA_FILE_SUFFIX" => "mysect", 
+				"AREA_FILE_RECURSIVE"=>"Y",
+				"EDIT_TEMPLATE" => "standard.php"
+				),
+				false
+			);?>
+			<!--include file for section END-->
 		</footer>
    </body>
 </html>
